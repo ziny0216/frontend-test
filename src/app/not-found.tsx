4efadc8b5b28b4edba05c292702e5c138918d458
ziny0216 <rkfwoa02@naver.com>
2025-04-21 15:16:19 +0900

    style: not found 페이지 스타일링 완료

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,10 +2,12 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/public">Return Home</Link>
-    </div>
+    <section className="not-found">
+      <div className="inner">
+        <h2>⚠️ Not Found</h2>
+        <p>Could not find requested resource</p>
+        <Link href="/">Return Home</Link>
+      </div>
+    </section>
   );
 }
